5bd5f4ba06ab1309b1799364b1974ba406633f83
Benita Daniel <benitadaniel.dev@gmail.com>
2023-07-11 00:47:06 +0100

    minor updates

--- src/assets/images-links.js
+++ src/assets/images-links.js
@@ -118,5 +118,69 @@ export const links = [
     label: "Lakefront",
     imgSrc:
       "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
+  }, {
+    label: "Rooms",
+    imgSrc:
+      "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+  },
+  {
+    label: "Amazing pools",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
+  },
+  {
+    label: "Trending",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+  },
+  {
+    label: "Beach",
+    imgSrc:
+      "https://a0.muscache.com/pictures/10ce1091-c854-40f3-a2fb-defc2995bcaf.jpg",
+  },
+  {
+    label: "Countryside",
+    imgSrc:
+      "	https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg",
+  },
+  {
+    label: "Mansions",
+    imgSrc:
+      "https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg",
+  },
+  {
+    label: "Amazing views",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+  },
+  {
+    label: "Iconic cities",
+    imgSrc:
+      "https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg",
+  },
+  {
+    label: "Domes",
+    imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+  },
+  {
+    label: "Top of the world",
+    imgSrc:
+      "https://a0.muscache.com/pictures/248f85bf-e35e-4dc3-a9a1-e1dbff9a3db4.jpg",
+  },
+  {
+    label: "OMG!",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "Lakefront",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "Lakefront",
+    imgSrc:
+      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
   },
 ];
